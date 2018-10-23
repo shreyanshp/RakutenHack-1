69e8b2fd275b1261645665c285eaae3d1d88ebfa
erick mendieta <erick84mm@gmail.com>
2018-10-23 13:58:58 +0900

    initial after clone

--- App.js
+++ App.js
@@ -3,11 +3,83 @@ import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-nativ
 
 import firebase from 'react-native-firebase';
 
+
+import {
+  createDrawerNavigator,
+  createStackNavigator,
+} from 'react-navigation';
+import { withRkTheme } from 'react-native-ui-kitten';
+import { AppRoutes } from './config/navigation/routesBuilder';
+import * as Screens from './screens';
+import { bootstrap } from './config/bootstrap';
+import { data } from './data';
+
+
+bootstrap();
+data.populateData();
+
+const SuperNannies = createStackNavigator({
+  First: {
+    screen: Screens.SplashScreen,
+  },
+  Home: {
+    screen: createDrawerNavigator(
+      {
+        ...AppRoutes,
+      },
+      {
+        contentComponent: (props) => {
+          const SideMenu = withRkTheme(Screens.SideMenu);
+          return <SideMenu {...props} />;
+        },
+      },
+    ),
+  },
+}, {
+  headerMode: 'none',
+});
+
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
+
+  state = {
+    isLoaded: false,
+  };
+
+  componentWillMount() {
+    this.loadAssets();
+  }
+
+  onNavigationStateChange = (previous, current) => {
+    const screen = {
+      current: this.getCurrentRouteName(current),
+      previous: this.getCurrentRouteName(previous),
+    };
+    if (screen.previous !== screen.current) {
+
+    }
+  };
+
+  getCurrentRouteName = (navigation) => {
+    const route = navigation.routes[navigation.index];
+    return route.routes ? this.getCurrentRouteName(route) : route.routeName;
+  };
+
+  loadAssets = async () => {
+    this.setState({ isLoaded: true });
+  };
+
+
+  renderApp = () => (
+    <View style={{ flex: 1 }}>
+      <SuperNannies onNavigationStateChange={this.onNavigationStateChange} />
+    </View>
+  );
+
+  render = () => (this.state.isLoaded ? this.renderApp() : this.renderApp());
 
   async componentDidMount() {
     // TODO: You: Do firebase things
@@ -17,50 +89,7 @@ export default class App extends React.Component {
     // await firebase.analytics().logEvent('foo', { bar: '123'});
   }
 
-  render() {
-    return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-          <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          {Platform.OS === 'ios' ? (
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
-          ) : (
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Cmd+M or shake for dev menu
-            </Text>
-          )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
-        </View>
-      </ScrollView>
-    );
-  }
+
 }
 
 const styles = StyleSheet.create({
